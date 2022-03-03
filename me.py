514569503c35854b36b606d972961f734d65b6e2
Richson Simbabure <richsonsimbabure@gmail.com>
2022-03-03 15:46:29 -0500

    added some comments to correct to make it understandable for the next user

--- me.py
+++ me.py
@@ -1,11 +1,13 @@
 import pyautogui as pg
 import time 
 
-time.sleep(10)
+time.sleep(10) ## you can change it to whatever time that you want
 txt = open('happbirthday.txt', 'r')
-a = "happy birthday" 
+a = "Name of person"# e.g you .. you can change this part to whatever you want or remove it 
 
 for i in txt:
     pg.write(a+'' +i)
     pg.press('Enter')
+
+    #to make it easy make sure you put your txt fie in the same folder with your python file
    
